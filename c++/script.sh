c09c420a9a643f280ce70c1e8d68ac9910e31abd
ncordon <nacho.cordon.castillo@gmail.com>
2019-02-28 21:52:56 +0100

    Modifies script to make executions in parallel

--- c++/script.sh
+++ c++/script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 g++ photos.cc -o photos
-(./photos < ../examples/a_example.txt) > ./a_example.out
-(./photos < ../examples/b_lovely_landscapes.txt) > ./b_lovely_landscapes.out
-(./photos < ../examples/c_memorable_moments.txt) > ./c_memorable_moments.out
-(./photos < ../examples/d_pet_pictures.txt) > ./d_pet_pictures.out
-(./photos < ../examples/e_shiny_selfies.txt) > ./e_shiny_selfies.out
+gnome-terminal -x bash -c -- "(./photos < ../examples/a_example.txt) > ./a_example.out"
+gnome-terminal -x bash -c -- "(./photos < ../examples/b_lovely_landscapes.txt) > ./b_lovely_landscapes.out"
+gnome-terminal -x bash -c -- "(./photos < ../examples/c_memorable_moments.txt) > ./c_memorable_moments.out"
+gnome-terminal -x bash -c -- "(./photos < ../examples/d_pet_pictures.txt) > ./d_pet_pictures.out"
+gnome-terminal -x bash -c -- "(./photos < ../examples/e_shiny_selfies.txt) > ./e_shiny_selfies.out"
